ed77217cc8f63b9affdbd4bd15bb59a1237af335
Awonugba Isaac Tolu <isaacawonugba@yahoo.com>
2018-04-19 00:04:50 +0100

    Add files via upload

---
+++ app/src/main/java/com/zeolink/med_manager/utility/ApplicationScope.java
@@ -0,0 +1,13 @@
+package com.zeolink.med_manager.utility;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by isaac on 4/16/2018.
+ */
+@Documented
+@Retention(RetentionPolicy.CLASS)
+public @interface ApplicationScope {
+}
